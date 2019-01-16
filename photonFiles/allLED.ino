ac9b0dced5014905190a1795925b240ceb4ffc8f
Edward Lu <lued2018@gmail.com>
2019-01-16 17:56:43 -0500

    Update allLED.ino

--- photonFiles/allLED.ino
+++ photonFiles/allLED.ino
@@ -1,3 +1,12 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <FastLED.h>
+FASTLED_USING_NAMESPACE
+
+#define LED_COUNT 60
+#define LED_PIN D4
+
+struct CRGB leds[LED_COUNT];
+
 int RED_PIN = D1;
 int GREEN_PIN = D2;
 int BLUE_PIN = D3;
@@ -6,21 +15,48 @@ int ledPin = D0;
 int changeColor(String param);
 int changeIntensity(String param);
 
+int changeColorStrip(String param);
+
 int color = 0;
 int ledIntensity = 0;
+int hue = 32;
+
+byte idex = 255;
+byte meteorLength = 29;
 
 void setup() {
+    LEDS.addLeds<WS2812, LED_PIN, GRB>(leds, LED_COUNT);
+    LEDS.setBrightness(200);
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
     pinMode(ledPin, OUTPUT);
     Particle.function("changeColor", changeColor);
     Particle.function("changeIntensity", changeIntensity);
+    Particle.function("changeColorStrip", changeColorStrip);
 }
 
 void loop() {
     displayColor(color);
     analogWrite(ledPin, ledIntensity);
+    allOn(hue);
+}
+
+void allOn(int hue){
+    if (hue != 0){
+        for (int i=0; i <= 59; i++)
+        {
+            leds[i] = CHSV(hue,200,200);
+        }
+        FastLED.show();
+    }
+    else{
+        for (int i=0; i <= 59; i++)
+        {
+            leds[i] = CHSV(0,0,0);
+        }
+        FastLED.show();
+    }
 }
 
 void displayColor(int color) {
@@ -67,9 +103,17 @@ void displayColor(int color) {
 int changeColor(String param){
     color = param.toInt();
     ledIntensity = 0;
+    hue = 0;
 }
 
 int changeIntensity(String param){
     ledIntensity = param.toInt();
+    color = 0;
+    hue = 0;
+}
+
+int changeColorStrip(String param){
+    hue = param.toInt();
+    ledIntensity = 0;
     color = 0;
 }
